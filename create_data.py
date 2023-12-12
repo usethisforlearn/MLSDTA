3eda1de5fa6a251f5f820e42f83d1996c04312c8
usethisforlearn <59911950+usethisforlearn@users.noreply.github.com>
2023-12-12 09:45:21 +0800

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,357 @@
+import pandas as pd
+import numpy as np
+import os
+import json,pickle
+from collections import OrderedDict
+
+import torch
+from rdkit import Chem
+from rdkit.Chem import MolFromSmiles
+import networkx as nx
+from utils import *
+
+
+
+def dic_normalize(dic):
+
+    max_value = dic[max(dic, key=dic.get)]
+    min_value = dic[min(dic, key=dic.get)]
+    interval = float(max_value) - float(min_value)
+    for key in dic.keys():
+        dic[key] = (dic[key] - min_value) / interval
+    dic['X'] = (max_value + min_value) / 2.0
+    return dic
+
+pro_res_table = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y','X']
+pro_res_aliphatic_table = ['A', 'I', 'L', 'M', 'V']
+pro_res_aromatic_table = ['F', 'W', 'Y']
+pro_res_polar_neutral_table = ['C', 'N', 'Q', 'S', 'T']
+pro_res_acidic_charged_table = ['D', 'E']
+pro_res_basic_charged_table = ['H', 'K', 'R']
+res_weight_table = {'A': 71.08, 'C': 103.15, 'D': 115.09, 'E': 129.12, 'F': 147.18, 'G': 57.05, 'H': 137.14,
+                    'I': 113.16, 'K': 128.18, 'L': 113.16, 'M': 131.20, 'N': 114.11, 'P': 97.12, 'Q': 128.13,
+                    'R': 156.19, 'S': 87.08, 'T': 101.11, 'V': 99.13, 'W': 186.22, 'Y': 163.18}
+
+res_pka_table = {'A': 2.34, 'C': 1.96, 'D': 1.88, 'E': 2.19, 'F': 1.83, 'G': 2.34, 'H': 1.82, 'I': 2.36,
+                 'K': 2.18, 'L': 2.36, 'M': 2.28, 'N': 2.02, 'P': 1.99, 'Q': 2.17, 'R': 2.17, 'S': 2.21,
+                 'T': 2.09, 'V': 2.32, 'W': 2.83, 'Y': 2.32}
+
+res_pkb_table = {'A': 9.69, 'C': 10.28, 'D': 9.60, 'E': 9.67, 'F': 9.13, 'G': 9.60, 'H': 9.17,
+                 'I': 9.60, 'K': 8.95, 'L': 9.60, 'M': 9.21, 'N': 8.80, 'P': 10.60, 'Q': 9.13,
+                 'R': 9.04, 'S': 9.15, 'T': 9.10, 'V': 9.62, 'W': 9.39, 'Y': 9.62}
+
+res_pkx_table = {'A': 0.00, 'C': 8.18, 'D': 3.65, 'E': 4.25, 'F': 0.00, 'G': 0, 'H': 6.00,
+                 'I': 0.00, 'K': 10.53, 'L': 0.00, 'M': 0.00, 'N': 0.00, 'P': 0.00, 'Q': 0.00,
+                 'R': 12.48, 'S': 0.00, 'T': 0.00, 'V': 0.00, 'W': 0.00, 'Y': 0.00}
+
+res_pl_table = {'A': 6.00, 'C': 5.07, 'D': 2.77, 'E': 3.22, 'F': 5.48, 'G': 5.97, 'H': 7.59,
+                'I': 6.02, 'K': 9.74, 'L': 5.98, 'M': 5.74, 'N': 5.41, 'P': 6.30, 'Q': 5.65,
+                'R': 10.76, 'S': 5.68, 'T': 5.60, 'V': 5.96, 'W': 5.89, 'Y': 5.96}
+
+res_hydrophobic_ph2_table = {'A': 47, 'C': 52, 'D': -18, 'E': 8, 'F': 92, 'G': 0, 'H': -42, 'I': 100,
+                             'K': -37, 'L': 100, 'M': 74, 'N': -41, 'P': -46, 'Q': -18, 'R': -26, 'S': -7,
+                             'T': 13, 'V': 79, 'W': 84, 'Y': 49}
+
+res_hydrophobic_ph7_table = {'A': 41, 'C': 49, 'D': -55, 'E': -31, 'F': 100, 'G': 0, 'H': 8, 'I': 99,
+                             'K': -23, 'L': 97, 'M': 74, 'N': -28, 'P': -46, 'Q': -10, 'R': -14, 'S': -5,
+                             'T': 13, 'V': 76, 'W': 97, 'Y': 63}
+res_weight_table = dic_normalize(res_weight_table)
+res_pka_table = dic_normalize(res_pka_table)
+res_pkb_table = dic_normalize(res_pkb_table)
+res_pkx_table = dic_normalize(res_pkx_table)
+res_pl_table = dic_normalize(res_pl_table)
+res_hydrophobic_ph2_table = dic_normalize(res_hydrophobic_ph2_table)
+res_hydrophobic_ph7_table = dic_normalize(res_hydrophobic_ph7_table)
+
+def residue_features(residue):
+    res_property1 = [1 if residue in pro_res_aliphatic_table else 0, 1 if residue in pro_res_aromatic_table else 0,
+                     1 if residue in pro_res_polar_neutral_table else 0,
+                     1 if residue in pro_res_acidic_charged_table else 0,
+                     1 if residue in pro_res_basic_charged_table else 0]
+    res_property2 = [res_weight_table[residue], res_pka_table[residue], res_pkb_table[residue], res_pkx_table[residue],
+                     res_pl_table[residue], res_hydrophobic_ph2_table[residue], res_hydrophobic_ph7_table[residue]]
+    # print(np.array(res_property1 + res_property2).shape)
+    return np.array(res_property1 + res_property2)
+
+
+# drug sequence------------------------------------------------------
+CHARISOSMISET = {"#": 29, "%": 30, ")": 31, "(": 1, "+": 32, "-": 33, "/": 34, ".": 2,
+				"1": 35, "0": 3, "3": 36, "2": 4, "5": 37, "4": 5, "7": 38, "6": 6,
+				"9": 39, "8": 7, "=": 40, "A": 41, "@": 8, "C": 42, "B": 9, "E": 43,
+				"D": 10, "G": 44, "F": 11, "I": 45, "H": 12, "K": 46, "M": 47, "L": 13,
+				"O": 48, "N": 14, "P": 15, "S": 49, "R": 16, "U": 50, "T": 17, "W": 51,
+				"V": 18, "Y": 52, "[": 53, "Z": 19, "]": 54, "\\": 20, "a": 55, "c": 56,
+				"b": 21, "e": 57, "d": 22, "g": 58, "f": 23, "i": 59, "h": 24, "m": 60,
+				"l": 25, "o": 61, "n": 26, "s": 62, "r": 27, "u": 63, "t": 28, "y": 64}
+
+CHARISOSMILEN = 64
+
+def label_smiles(line, MAX_SMI_LEN, smi_ch_ind):
+	X = np.zeros(MAX_SMI_LEN)
+	for i, ch in enumerate(line[:MAX_SMI_LEN]): #	x, smi_ch_ind, y
+		X[i] = smi_ch_ind[ch]
+
+	return X #.tolist()
+
+#drug graph------------------------------------------------------------------------
+
+def atom_features(atom):
+    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
+                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
+                    [atom.GetIsAromatic()])
+
+
+def one_of_k_encoding(x, allowable_set):
+    if x not in allowable_set:
+        raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
+    return list(map(lambda s: x == s, allowable_set))
+
+
+def one_of_k_encoding_unk(x, allowable_set):
+    """Maps inputs not in the allowable set to the last element."""
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: x == s, allowable_set))
+
+
+def smile_to_graph(smile):
+    mol = Chem.MolFromSmiles(smile)
+    c_size = mol.GetNumAtoms()
+    features = []
+    
+    for atom in mol.GetAtoms():
+        feature = atom_features(atom)
+        features.append( feature / sum(feature) )
+    
+    edges = []
+    for bond in mol.GetBonds():
+        edges.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
+    g = nx.Graph(edges).to_directed()
+    edge_index = []
+    for e1, e2 in g.edges:
+        edge_index.append([e1, e2])
+        
+    return c_size, features, edge_index
+
+# target sequence--------------------------------------------------------
+def seq_cat(prot):
+    x = np.zeros(max_seq_len)
+    for i, ch in enumerate(prot[:max_seq_len]): 
+        x[i] = seq_dict[ch]
+    return x
+
+# target graph from DgraphDTA---------------------------------------------------------
+
+
+
+
+def PSSM_calculation(aln_file, pro_seq):
+    # 蛋白质特征矩阵
+    pfm_mat = np.zeros((len(pro_res_table), len(pro_seq)))
+    with open(aln_file, 'r') as f:
+        line_count = len(f.readlines())
+        for line in f.readlines():
+            if len(line) != len(pro_seq):
+                print('error', len(line), len(pro_seq))
+                continue
+            count = 0
+            for res in line:
+                if res not in pro_res_table:
+                    count += 1
+                    continue
+                
+                pfm_mat[pro_res_table.index(res), count] += 1
+                count += 1
+    # ppm_mat = pfm_mat / float(line_count)
+    pseudocount = 0.8
+    ppm_mat = (pfm_mat + pseudocount / 4) / (float(line_count) + pseudocount)
+    pssm_mat = ppm_mat
+    # k = float(len(pro_res_table))
+    # pwm_mat = np.log2(ppm_mat / (1.0 / k))
+    # pssm_mat = pwm_mat
+    # print(pssm_mat)
+    return pssm_mat
+
+
+# target sequence feature
+def seq_feature(pro_seq):
+    pro_hot = np.zeros((len(pro_seq), len(pro_res_table)))
+    pro_property = np.zeros((len(pro_seq), 12))
+    for i in range(len(pro_seq)):
+        # if 'X' in pro_seq:
+        #     print(pro_seq)
+        pro_hot[i,] = one_of_k_encoding(pro_seq[i], pro_res_table)
+        pro_property[i,] = residue_features(pro_seq[i])
+    return np.concatenate((pro_hot, pro_property), axis=1)
+
+def target_feature(aln_file, pro_seq):
+    pssm = PSSM_calculation(aln_file, pro_seq)
+    other_feature = seq_feature(pro_seq)
+    # print('target_feature')
+    # print(pssm.shape)
+    # print(other_feature.shape)
+
+    # print(other_feature.shape)
+    # return other_feature
+    return np.concatenate((np.transpose(pssm, (1, 0)), other_feature), axis=1)
+
+def target_to_feature(target_key, target_sequence, aln_dir):
+    # aln_dir = 'data/' + dataset + '/aln'
+    aln_file = os.path.join(aln_dir, target_key + '.aln')
+    # if 'X' in target_sequence:
+    #     print(target_key)
+    feature = target_feature(aln_file, target_sequence)
+    return feature
+
+
+
+def target_to_graph(target_key, target_sequence, contact_dir, aln_dir):
+    target_edge_index = []
+    target_size = len(target_sequence)
+    # contact_dir = 'data/' + dataset + '/pconsc4'
+    contact_file = os.path.join(contact_dir, target_key + '.npy')
+    contact_map = np.load(contact_file)
+    contact_map += np.matrix(np.eye(contact_map.shape[0]))
+    index_row, index_col = np.where(contact_map >= 0.5)
+    for i, j in zip(index_row, index_col):
+        target_edge_index.append([i, j])
+    target_feature = target_to_feature(target_key, target_sequence, aln_dir)
+    target_edge_index = np.array(target_edge_index)
+
+    return target_size, target_feature, target_edge_index
+
+def valid_target(key, dataset):
+    contact_dir = 'data/' + dataset + '/pconsc4'
+    aln_dir = 'data/' + dataset + '/aln'
+    contact_file = os.path.join(contact_dir, key + '.npy')
+    aln_file = os.path.join(aln_dir, key + '.aln')
+    if os.path.exists(contact_file) and os.path.exists(aln_file):
+        return True
+    else:
+        return False
+
+
+
+#-create drug sequence, drug graph, target sequence , target graph
+
+
+seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}  # encode alphabet from 1
+seq_dict_len = len(seq_dict)
+max_seq_len = 1000
+
+
+
+
+def create_dataset(dataset,fold=0):
+    # ------------------------------create  CSV file-------------------------------------------
+    print('convert data from DeepDTA for ', dataset)
+    fpath = 'data/' + dataset + '/'
+    train_fold = json.load(open(fpath + "folds/train_fold_setting1.txt"))  # len=5
+    train_fold = [ee for e in train_fold for ee in e]  # davis len=25046
+    valid_fold = json.load(open(fpath + "folds/test_fold_setting1.txt"))  # davis len=5010
+    ligands = json.load(open(fpath + "ligands_can.txt"), object_pairs_hook=OrderedDict)  # davis len=68
+    proteins = json.load(open(fpath + "proteins.txt"), object_pairs_hook=OrderedDict)  # davis len=442
+    affinity = pickle.load(open(fpath + "Y", "rb"), encoding='latin1')  # davis len=68
+
+    msa_path = 'data/' + dataset + '/aln'
+    contac_path = 'data/' + dataset + '/pconsc4'
+    msa_list = []
+    contact_list = []
+    for key in proteins:
+        msa_list.append(os.path.join(msa_path, key + '.aln'))
+        contact_list.append(os.path.join(contac_path, key + '.npy'))
+    drugs = []
+    prots = []
+    prot_keys = []
+    drug_smiles = []
+    # smiles
+    for d in ligands.keys():
+        lg = Chem.MolToSmiles(Chem.MolFromSmiles(ligands[d]), isomericSmiles=True)
+        drugs.append(lg)  # loading drugs
+        drug_smiles.append(ligands[d])
+    # targets
+    for t in proteins.keys():
+        prots.append(proteins[t])  # loading proteins
+        prot_keys.append(t)
+
+    if dataset == 'davis':
+        affinity = [-np.log10(y / 1e9) for y in affinity]
+    affinity = np.asarray(affinity)  # affinity shape=(68 drug,442 prot)
+
+    opts = ['train', 'test']
+
+    for opt in opts:
+        rows, cols = np.where(np.isnan(affinity) == False)  # not NAN
+        if opt == 'train':
+            rows, cols = rows[train_fold], cols[train_fold]  # train fold包含了drug和prot的index信息
+        elif opt == 'test':
+            rows, cols = rows[valid_fold], cols[valid_fold]
+        with open('data/' + dataset + '_' + opt + '.csv', 'w') as f:
+            f.write('compound_iso_smiles,target_key,target_sequence,affinity\n')
+            for pair_ind in range(len(rows)):
+                if not valid_target(prot_keys[cols[pair_ind]], dataset):  # 判断key是否存在 aln 和 pconsc4 文件
+                    continue
+                ls = []
+                ls += [drugs[rows[pair_ind]]]
+                ls += [prot_keys[cols[pair_ind]]]
+                ls += [prots[cols[pair_ind]]]
+                ls += [affinity[rows[pair_ind], cols[pair_ind]]]
+                f.write(','.join(map(str, ls)) + '\n')  # csv format
+    print('\ndataset:', dataset)
+    print('train_fold:', len(train_fold))
+    print('test_fold:', len(valid_fold))
+    print('len(set(drugs)):', len(set(drugs)),'---len(set(prots)):', len(set(prots)))
+    # all_prots += list(set(prots))
+    print('finish',dataset,' csv file')
+
+    compound_iso_smiles = drugs
+    target_key = prot_keys
+
+    smile_graph = {}
+    for smile in compound_iso_smiles:
+        g = smile_to_graph(smile)
+        smile_graph[smile] = g
+    print('finish drug graph')
+    smile_tensor = {}
+    for smile in compound_iso_smiles:
+        smi_tensor = label_smiles(smile, 100, CHARISOSMISET)
+        smile_tensor[smile] = smi_tensor
+    print('finish drug sequence')
+
+    target_graph = {}
+    for key in target_key:
+        if not valid_target(key, dataset):  # ensure the contact and aln files exists
+            continue
+        g = target_to_graph(key, proteins[key], contac_path, msa_path)
+        target_graph[key] = g
+    print('finish target graph')
+
+    if len(target_graph) == 0:
+        raise Exception('没有 aln 文件和 pconsc4文件。')
+    df = pd.read_csv('data/' + dataset + '_train.csv')
+    train_drugs, train_prots,  train_Y = list(df['compound_iso_smiles']),list(df['target_sequence']),list(df['affinity'])
+    train_prot_keys = np.asarray(list(df['target_key']))
+    XT = [seq_cat(t) for t in train_prots]
+
+    train_drugs, train_prots,  train_Y = np.asarray(train_drugs), np.asarray(XT), np.asarray(train_Y)
+
+    df = pd.read_csv('data/' + dataset + '_test.csv')
+    test_drugs, test_prots,  test_Y = list(df['compound_iso_smiles']),list(df['target_sequence']),list(df['affinity'])
+    test_prot_keys = np.asarray(list(df['target_key']))
+    XT = [seq_cat(t) for t in test_prots]
+
+    test_drugs, test_prots,  test_Y = np.asarray(test_drugs), np.asarray(XT), np.asarray(test_Y)
+
+    # make data PyTorch Geometric ready
+
+    print('ready for train_data and test_data')
+    train_data = TestbedDataset(root='data', dataset=dataset+'_train', xd=train_drugs, xt=train_prots,
+                                y=train_Y, smile_graph=smile_graph,smile_tensor=smile_tensor,target_graph = target_graph,target_key=train_prot_keys)
+    test_data = TestbedDataset(root='data', dataset=dataset+'_test', xd=test_drugs, xt=test_prots,
+                               y=test_Y,smile_graph=smile_graph,smile_tensor=smile_tensor,target_graph = target_graph,target_key=test_prot_keys)
+    print('finish train_data and test_data')
+
+    return train_data , test_data
